d008458fa19c7fa1702e02bc55022fc9861df9d2
damien <contact.damiendiaz@gmail.com>
2022-10-08 09:08:41 +0200

    navbar

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    /**
+     * @Route ("/", name="app_accueil")
+     */
+    public function index(): Response
+    {
+        $firstname = "Damien";
+
+        return $this->render('accueil/index.html.twig', [
+            'firstname' => $firstname
+        ]);
+    }
+}
